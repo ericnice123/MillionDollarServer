31a1841512facff502d36516f0a27029d930f86f
EricTsai <etsai@PowerUsers-MBP.attlocal.net>
2019-01-03 23:36:05 -0800

    adding static test json

--- app.js
+++ app.js
@@ -11,9 +11,33 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Static test json
+var costElementJson = {
+    "costElements": [
+        {id: 1, color: '#F44336', costAmount: 123},
+        {id: 2, color: '#2196F3', costAmount: 456},
+        {id: 3, color: '#FFEB3B', costAmount: 789},
+        {id: 4, color: '#4CAF50', costAmount: 675},
+        {id: 5, color: '#FF9800', costAmount: 999},
+        {id: 6, color: '#FACE00', costAmount: 666}
+    ]
+}
+
+var costElementArray = [
+    {id: 1, color: '#F44336', costAmount: 123},
+    {id: 2, color: '#2196F3', costAmount: 456},
+    {id: 3, color: '#FFEB3B', costAmount: 789},
+    {id: 4, color: '#4CAF50', costAmount: 675},
+    {id: 5, color: '#FF9800', costAmount: 999},
+    {id: 6, color: '#FACE00', costAmount: 666}
+]
 
 app.get('/', function(req, res){
     res.send('Hello world');
+});
+
+app.get('/testJson', function(req, res){
+    res.send(costElementJson);
 });
 
 app.listen(3000, function(){
